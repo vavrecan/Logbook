814632836447faa5bdf123ab02442818bbf89fee
Marek Vavrečan <vavrecan@gmail.com>
2015-06-02 17:51:31 +0100

    initial commit

---
+++ ServerInterface.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerTypes;
+
+namespace ServerInterface
+{
+    public interface IServerAdministrationInterface
+    {
+        Boolean Started(String Password);
+        void Start(String Password);
+        void Stop(String Password);
+        List<AdminSession> GetSessions(String Password);
+        List<AdminSystemEvent> GetSystemEvents(String Password, AdminSystemEvent LastItem);
+
+        Boolean CheckLogin(String Password);
+
+        void SetDBPath(String Password, String DBPath);
+        void SetPort(String Password, Int32 Port);
+        void SetTimeout(String Password, Int32 Timeout);
+        void Vacuum(String Password);
+
+        String GetDBPath(String Password);
+        Int32 GetPort(String Password);
+        Int32 GetTimeout(String Password);
+
+        List<AdminEmployees> GetEmployees(String Password);
+        List<Permission> GetEmployeePermissions(String Password, Int32 Employee);
+
+        void CleanSessions(String Password);
+        void ChangePassword(String OldPassword, String NewPassword);
+        void NewEmployee(String Password, String Employee, String EmployeePassword, Boolean Active, Int32 EmployeeType);
+        void ChangeEmployeePassword(String Password, Int32 employee, String NewPassword);
+        void DeleteEmployee(String Password, Int32 Employee);
+        void SetEmployeeAccess(String Password, Int32 Employee, Boolean Access);
+        void SetEmployeePermissions(String Password, Int32 Employee, List<Permission> permissions);
+        void ResetEmployeePermissions(String Password, Int32 Employee);
+
+		List<Company> GetCompanies(String Password);
+
+		void NewCompany(String Password, String Name);
+		void DeleteCompany(String Password, Int32 Company);
+		void ChangeCompanyName(String Password, Int32 Company, String Name);
+
+		Company GetUnloggedUserCompany(String Password);
+		void SetUnloggedUserCompany(String Password, Int32 Company);
+
+		List<Company> GetEmployeeCompanies(String Password, Int32 Employee);
+		void SetEmployeeCompanies(String Password, Int32 Employee, List<Company> Companies);
+
+        List<AdminDatabaseFiles> GetDatabases(String Password);
+        string GetChecksum(String Password, String FileName);
+
+        byte [] DownloadChunk(String Password, String FileName, Int32 ChunkOffset);
+        Int32 DownloadChunkLength(String Password, String FileName);
+        Int32 DownloadFileLength(String Password, String FileName);
+
+        void UploadPrepare(String Password, String FileName);
+        void UploadChunk(String Password, String FileName, byte[] Data);
+        void UploadFinalize(String Password, String FileName, String CheckSum);
+
+        List<TimeExport> GetTimeExports(String Password);
+        void ExportTime(String Password, Int32 Year);
+        void ClearTime(String Password, Int32 Year);
+
+		byte[] GetCompanyLogo(String Password, Int32 Company);
+		void SetCompanyLogo(String Password, Int32 Company, byte[] image);
+		void DeleteCompanyLogo(String Password, Int32 Company);
+    }
+
+    public interface IServerInterface 
+    {
+        String GetSession(String UserName, String ComputerName, String RealName);
+		Company GetCompany(String Session);
+		byte[] GetCompanyLogo(String Session);
+
+        Boolean IsSessionValid(String hash);
+        Boolean Ping(String Session);
+        List<ConnectedUser> GetConnectedUsers();
+        List<LoginEmployee> GetEmployees(Boolean CanAccess);
+        List<Permission> GetPermissions(String Session);
+
+        Boolean LoginEmployee(String Session, String UserName, Int32 Group, String Password, Int32 Company);
+        void LogoutEmployee(String Session);
+        void ChangePassword(String Session, String OldPassword, String NewPassword);
+        void LockDatabase(String Session);
+
+        List<Event> GetEvents(String Session, Events.ReadFormat Format, object Argument, out Int32 MaxID, Events.ReadFilter Filter, out Boolean IsMin, out Boolean IsMax);
+        String LastEventEmployee(String Session);
+		Int32 GetRows(String Session, Events.ReadFilter Filter);
+        Int16 GetLastTurn(String Session);
+
+        void AddEvent(String Session, String Data, DateTime EventDate, Int32 Type, Int16 Turn);
+        void DeleteEvent(String Session, Int32 ID);
+        void LockEvent(String Session, Int32 ID);
+        void UpdateEvent(String Session, Event ev);
+        void MarkupEvent(String Session, Int32 ID, Int32 Mark);
+
+        List<SubEvent> GetSubEvents(String Session, List<Int32> parentID);
+        void AddSubEvent(String Session, SubEvent cm);
+        void DeleteSubEvent(String Session, Int32 ID);
+        void UpdateSubEvent(String Session, SubEvent cm);
+
+        List<Failure> GetFailures(String Session, Failures.ReadFormat format, Failures.Search Search, Events.Range range);
+		Int32 GetRowsFailures(String Session, Failures.ReadFormat format, Failures.Search Search);
+        Int32 GetRowsFailuresInProgress(String Session);
+
+        void AddFailure(String Session, Failure failure);
+        void SetFailureFix(String Session, Int32 ID, Int32 fix);
+        void DeleteFailure(String Session, Int32 ID);
+        void UpdateFailure(String Session, Failure failure);
+        void NoticeFailure(String Session, Int32 ID);
+
+        List<StoredInlineTable> GetInlineTables(String Session);
+        void AddInlineTable(String Session, StoredInlineTable sTable);
+        void RemoveInlineTable(String Session, String Name);
+    }
+}
---
+++ ServerTypes.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace ServerTypes
+{
+    [Serializable]
+    public class AdminEmployees
+    {
+        public Int32 id;
+        public string user;
+        public bool active;
+		public Int32 type;
+		public List<Company> companies;
+    }
+
+    [Serializable]
+    public class AdminSession
+    {
+        public string username;
+        public string pcname;
+        public string realname;
+        public DateTime activity;
+        public string loggeduser;
+        public Int32 loggedusergroup;
+    }
+
+    [Serializable]
+    public class AdminSystemEvent
+    {
+        public Int32 id;
+        public DateTime date;
+        public string text;
+        public Int32 type;
+    }
+
+    [Serializable]
+    public class AdminDatabaseFiles
+    {
+        public string filename;
+        public Int64 size;
+        public DateTime created;
+    }
+
+    [Serializable]
+    public class ConnectedUser
+    {
+        public string userName;
+        public string pcName;
+        public string ownerName;
+		public string company;
+    }
+
+    [Serializable]
+    public class LoginEmployee
+    {
+        public string userName;
+        public Int32 cathegory;
+        public bool canAccess;
+		public List<Company> companies;
+    }
+
+	[Serializable]
+	public class Company
+	{
+		public Int32 id;
+		public string name;
+	}
+
+    [Serializable]
+    public class Event
+    {
+        public Int32 id;
+        public string data;
+        public DateTime added;
+        public DateTime date;
+        public bool locked;
+        public Int32 type;
+        public string owner;
+        public short turn;
+        public short mark;
+    }
+
+    [Serializable]
+    public class Failure
+    {
+        public Int32 id;
+        public string name;
+        public string owner;
+        public string code;
+        public string type;
+        public string cause;
+        public string description;
+        public string notice;
+        public Int32 level;
+        public Int32 fix;
+        public Boolean inprogress;
+        public Boolean noticed;
+        public DateTime noticedate;
+        public DateTime start;
+        public DateTime end;
+    }
+
+    [Serializable]
+    public class SubEvent
+    {
+        public Int32 id;
+        public string data;
+        public DateTime added;
+        public string owner;
+        public Int32 parent;
+        public Int32 type;
+        public short mark;
+    }
+
+    [Serializable]
+    public class InlineTable
+    {
+        public String Summary;
+        public List<InlineTableRow> Rows;
+        public List<String> RowsFormat;
+        public List<String> RowsType;
+        public InlineTable NextTable;
+    }
+
+    [Serializable]
+    public class InlineTableRow
+    {
+        public List<String> Cells;
+    }
+
+    [Serializable]
+    public struct StoredInlineTable
+    {
+        public InlineTable table;
+        public String name;
+    }
+
+    [Serializable]
+    public class NewTurn
+    {
+        public String Given;
+        public String Received;
+        public List<String> Avalible;
+        public List<String> AvalibleOthers;
+    }
+
+    [Serializable]
+    public class TimeExport
+    {
+        public Int32 Year;
+        public Int32 EventsCount;
+    }
+
+    [Serializable]
+    public static class Failures
+    {
+        public static string[] FailuresFixTranslated = 
+        {             
+            "Pri plánovanej odstávke",
+            "V prípade výpadku celku",
+            "Podľa voľných kapacít",
+            "Do požadovaného termínu",
+            "Okamžite"
+        };
+
+        public enum ReadFormat
+        {
+            Normal,
+            Unnoticed,
+            InProgress,
+            LastTwoDays,
+            Search
+        }
+        
+        [Serializable]
+        public struct Search
+        {
+            public string searchText;
+            public string fromOwner;
+
+            public bool includeCode;
+            public bool includeType;
+            public bool includeName;
+            public bool includeCause;
+            public bool includeDescription;
+
+            public bool searchMinDate;
+            public bool searchMaxDate;
+            public DateTime minDate;
+            public DateTime maxDate;
+
+            public Int32 limit;
+        }
+    }
+
+    [Serializable]
+    public static class Events
+    {
+        public enum ReadFormat
+        {
+            LastTwoDays,
+            UnlockedItems,
+            SelectedDay,
+            Range,
+            Search,
+            Turn,
+            Selection
+        }
+
+        public enum ReadFilter
+        {
+            Normal,
+            OnlySubEvents,
+            OnlyMarked,
+            OnlyMarkedSubEvents,
+            OnlyEvents,
+            OnlyTurnSwitchs,
+            OnlyTables
+        }
+
+        [Serializable]
+        public struct IDRange
+        {
+            public Int32 start;
+            public Int32 end;
+        }
+
+        [Serializable]
+        public struct Range
+        {
+            public Int32 start;
+            public Int32 lenght;
+        }
+
+        [Serializable]
+        public struct TurnWay
+        {
+            public Int32 id;
+            public Boolean back;
+        }
+
+        [Serializable]
+        public struct Search
+        {
+            public string searchText;
+            public string fromOperator;
+            public bool searchMinDate;
+            public bool searchMaxDate;
+            public DateTime minDate;
+            public DateTime maxDate;
+            public bool includeComments;
+            public bool includeCommands;
+            public bool includeEvents;
+            public bool[] searchTurns;
+            public Int32 limit;
+        }
+    }
+
+    public static class Employees
+    {
+        public enum EmployeesCathegory
+        {
+            MainOperator,
+            SubOperator,
+            WatchDog,
+            Other,
+            Administrator,
+			Dispatcher,
+        }
+
+        public static string[] EmployeesTranslated = 
+        {
+            "Zmenový inžinier",
+            "Operátor",
+            "Kontrola", 
+            "Nezaradený",
+            "Správca",
+			"Dispečer"
+        };
+    }
+        
+    public enum Permission
+    {
+        EventsRead,
+        EventsLock,
+        EventsAdd,
+        EventsDelete,
+        EventsUpdate,
+        EventsMark,
+        EventsAddComment,
+        EventsAddCommand,
+        EventsDeleteComment,
+        EventsDeleteCommand,
+        EventsUpdateComment,
+        EventsUpdateCommand,
+        FailuresRead,
+        FailuresAdd,
+        FailuresDelete,
+        FailuresSetFix,
+        FailuresNotice,
+        FailuresUpdate,
+        OthersChangePassword,
+        InlineTableRead,
+        InlineTableAdd,
+        InlineTableDelete,
+        None
+    }
+    
+    [Serializable]
+    public static class Permissions
+    {
+        public static Dictionary<string, string> PermissionTranslated = new Dictionary<string,string>()
+        {
+            {Permission.EventsRead.ToString(),           "Zobraziť"},
+            {Permission.EventsLock.ToString(),           "Zamknúť"},
+            {Permission.EventsAdd.ToString(),            "Pridať"},
+            {Permission.EventsDelete.ToString(),         "Zmazať"},
+            {Permission.EventsUpdate.ToString(),         "Upraviť"},
+            {Permission.EventsMark.ToString(),           "Zvýrazniť"},
+            {Permission.EventsAddComment.ToString(),     "Pridať poznámku"},
+            {Permission.EventsAddCommand.ToString(),     "Pridať príkaz"},
+            {Permission.EventsDeleteComment.ToString(),  "Zmazať poznámku"},
+            {Permission.EventsDeleteCommand.ToString(),  "Zmazať príkaz"},
+            {Permission.EventsUpdateComment.ToString(),  "Upraviť poznámku"},
+            {Permission.EventsUpdateCommand.ToString(),  "Upraviť príkaz"},
+            {Permission.FailuresRead.ToString(),         "Zobraziť"},
+            {Permission.FailuresAdd.ToString(),          "Pridať"},
+            {Permission.FailuresDelete.ToString(),       "Zmazať"},
+            {Permission.FailuresSetFix.ToString(),       "Nastaviť opravu"},
+            {Permission.FailuresNotice.ToString(),       "Zobrať na vedomie"},
+            {Permission.FailuresUpdate.ToString(),       "Upraviť"},
+            {Permission.OthersChangePassword.ToString(), "Zmeniť heslo"},
+            {Permission.InlineTableRead.ToString(),      "Zobraziť"},
+            {Permission.InlineTableAdd.ToString(),       "Pridať"},
+            {Permission.InlineTableDelete.ToString(),    "Zmazať"}
+        };
+    }
+}
